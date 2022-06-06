792827b06b3b0b3fbc8b3a0498daeb32874cc6ec
komarov <komaroff.ilya.s@gmail.com>
2022-06-06 21:35:56 +0300

    init commit

---
+++ first_project/app/views.py
@@ -0,0 +1,31 @@
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from datetime import datetime
+import os
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    workdir = '; '.join(os.listdir())
+    msg = f'Cодержимое рабочей директории: {workdir}'
+    return HttpResponse(msg)
+
